35df72caf17d67a83f0b1f013a482730bdaea812
npipliza <piplizanadia@gmail.com>
2021-09-19 23:47:07 -0300

    Finalizado

---
+++ TP_1_Pipliza_2A/MiCalculadora/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Windows.Forms;
+
+namespace MiCalculadora
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal de la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormCalculadora());
+        }
+    }
+}
